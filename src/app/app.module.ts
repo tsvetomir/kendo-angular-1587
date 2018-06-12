77334f6ad6865afbd200e8ad959b1ac2d26a1a36
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2018-06-12 14:17:08 +0300

    chore: add test scenario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { PDFExportModule } from '@progress/kendo-angular-pdf-export';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PDFExportModule
   ],
   providers: [],
   bootstrap: [AppComponent]
